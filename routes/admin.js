ef4053e48c871d8ae0354352b06575b70298982d
Maulik Dholariya <maulikdholariya14@gmail.com>
2022-07-02 18:57:12 +0530

    first commit

---
+++ routes/admin.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const router = express.Router();
+const productController = require('../controllers/products');
+
+// /admin/add-product => GET
+router.get('/add-product', productController.getAddProduct);
+
+// /admin/add-product => POST
+router.post('/add-product', productController.postAddproduct);
+
+module.exports = router;
