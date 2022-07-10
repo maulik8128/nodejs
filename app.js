c26018d322fa7b992d6b92b17fbc66afc1cb25a2
Maulik Dholariya <maulikdholariya14@gmail.com>
2022-07-10 10:41:33 +0530

    mongoose

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 const mongoConnect = require('./util/database').mongoConnect;
 const User = require('./models/user');
 
@@ -16,14 +17,31 @@ const Router1 = require('./routes/routes');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
-  User.findById('62c93054263a45797dede7ff')
+  User.findById('62c974cc3b91d9970a1a6e31')
     .then((user) => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
+      req.user = user;
       next();
     })
     .catch((err) => console.log(err));
 });
 Router1(app);
-mongoConnect(() => {
-  app.listen(3000);
-});
+
+mongoose
+  .connect('mongodb://localhost:27017/nodejs')
+  .then((result) => {
+    User.findOne().then((user) => {
+      if (!user) {
+        const user = new User({
+          name: 'test',
+          email: 'test@gmail.com',
+          cart: {
+            items: [],
+          },
+        });
+        user.save();
+      }
+    });
+    console.log('connected!!');
+    app.listen(3000);
+  })
+  .catch((err) => console.log(err));
