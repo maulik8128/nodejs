07d32f9e3f8e3582df6a4fa531ee8763ada2a775
Maulik Dholariya <maulikdholariya14@gmail.com>
2022-07-16 17:26:12 +0530

    add cs and csrf

--- app.js
+++ app.js
@@ -6,18 +6,46 @@ const morgan = require('morgan');
 const mongoose = require('mongoose');
 const mongoConnect = require('./util/database').mongoConnect;
 const User = require('./models/user');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+const csrf = require('csurf');
+const flash = require('connect-flash');
 
+const MONGODB_URI = 'mongodb://localhost:27017/nodejs';
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions',
+});
+const csrfProtection = csrf();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms')
 );
-const Router1 = require('./routes/routes');
+app.use(
+  session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+  })
+);
+app.use(csrfProtection);
+app.use(flash());
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
+const Router1 = require('./routes');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
-  User.findById('62c974cc3b91d9970a1a6e31')
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
     .then((user) => {
       req.user = user;
       next();
@@ -27,20 +55,20 @@ app.use((req, res, next) => {
 Router1(app);
 
 mongoose
-  .connect('mongodb://localhost:27017/nodejs')
+  .connect(MONGODB_URI)
   .then((result) => {
-    User.findOne().then((user) => {
-      if (!user) {
-        const user = new User({
-          name: 'test',
-          email: 'test@gmail.com',
-          cart: {
-            items: [],
-          },
-        });
-        user.save();
-      }
-    });
+    // User.findOne().then((user) => {
+    //   if (!user) {
+    //     const user = new User({
+    //       name: 'test',
+    //       email: 'test@gmail.com',
+    //       cart: {
+    //         items: [],
+    //       },
+    //     });
+    //     user.save();
+    //   }
+    // });
     console.log('connected!!');
     app.listen(3000);
   })
